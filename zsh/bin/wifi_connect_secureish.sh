380dbf14fca5853e1784e3675b6bf44de8c0ce66
jesse <jesse.koekkoek@incentro.com>
2022-06-03 13:43:10 +0200

    Learned that accepting spaces in bash requires quoting variables. Finally fixed wificonnect x)

--- zsh/bin/wifi_connect_secureish.sh
+++ zsh/bin/wifi_connect_secureish.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 echo "Network name";
-read network;
+read -e network;
 
 echo "Password";
 read -s pass;
 
+echo
 echo "Connecto to ze network $network";
 
-nmcli d wifi connect $network password $pass
-exit 0
+nmcli d wifi connect "$network" password $pass
+# exit 0
